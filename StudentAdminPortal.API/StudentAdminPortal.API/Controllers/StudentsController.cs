1dfeee2624288e15f0595d51eb6d5c95fbf49ce7
khangbui <khangbuionlinegmail.com>
2022-08-30 22:15:22 +0700

    Done section 2 API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await studentRepository.GetStudentsAsync();
+
+            
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DataModels = StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.DomainModels;
+
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Student, Student>().ReverseMap();
+
+            CreateMap<Gender, Gender>().ReverseMap();
+
+            CreateMap<Address, Address>().ReverseMap();
+        }
+    }
+}
